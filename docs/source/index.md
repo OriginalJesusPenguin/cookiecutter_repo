2906d65b56335455607773999bc235a6ead6c985
Rami <ralbe@dtu.dk>
2025-03-30 16:18:37 +0200

    initial cookiecutter commit

---
+++ docs/source/index.md
@@ -0,0 +1,3 @@
+## Documentation
+
+Documentation for rami_project
---
+++ src/rami_project/train.py
@@ -0,0 +1,10 @@
+from rami_project.model import Model
+from rami_project.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
